5e83c60f0d595a6e8b8066818934ddf9bcf07f97
Sewi12 <Sewi12@gmx.at>
2026-09-25 09:08:24 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE='baustellenzeit-v8-cloud';
+const CACHE='baustellenzeit-v9';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./supabase-config.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
