89b295d1d71b488c833c030b2bdca456ecfa6bcc
Sewi12 <Sewi12@gmx.at>
2026-09-25 09:22:24 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baustellenzeit-v9';
+const CACHE='baustellenzeit-v9-2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./supabase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
